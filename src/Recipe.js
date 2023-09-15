7dee3f1f7d0aed8f89cddab0613e3fe062e0f8c6
Justin Kawaja <jkawaja909@gmail.com>
2023-09-15 10:10:38 -0230

    coding for image, created new folder for images

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,13 +1,14 @@
 export function RecipeList({ recipes }) {
   return recipes.map((recipe, i) => {
-    return <Recipe name={recipe.name} />;
+    return <Recipe name={recipe.name} image={recipe.image} />;
   });
 }
 
-function Recipe({ name }) {
+function Recipe({ name, image }) {
   return (
     <div>
       <p>{name}</p>
+      <img src={image} />
     </div>
   );
 }
