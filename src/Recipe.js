84bc011e7eca60ba0a02c92a284f792c6be0fdda
Justin Kawaja <jkawaja909@gmail.com>
2023-09-21 20:21:36 -0230

    added new onClick, removed old version

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,42 +1,50 @@
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom"
 
-export function RecipeList({ recipes }) {
-  return recipes.map((recipe, setRecipes, i) => {
+export function RecipeList({ recipes, setRecipes }) {
+  return recipes.map((recipe, i) => {
     return (
     <div>
-      <nav>
-        <Link to="/AddRecipe">Add Recipe</Link>
-      </nav>
       <Recipe 
-        id={i}
+        key={i}
         name={recipe.name}
         ingredients={recipe.ingredients}
         directions={recipe.directions}
         description={recipe.description} 
         image={recipe.image} 
         setRecipes={setRecipes}
+        recipes={recipes}
       />
     </div>
   )});
 }
 
-
-
-function Recipe({ id, name, ingredients, directions, description, image }) {
-  return (  
-    <div>    
-      <div id={id}>
-          <h3>{name}</h3>
-            <ul>
-              <li>{description}</li>
-              <li>Ingredients: {ingredients}</li>
-              <li>Directions: {directions}</li>
-            </ul>
-          <img height={200} src={image} alt="This is a dish."/><br/>
-        <button onClick={() => } type="button">REMOVE</button>
-        </div>
+function Recipe({ name, ingredients, directions, description, image, recipes, setRecipes}) {
+  return ( 
+    <div>
+        <h3>{name}</h3>
+          <ul>
+            <li>{description}</li>
+            <li>Ingredients: {ingredients}</li>
+            <li>Directions: {directions}</li>
+          </ul>
+        <img height={200} src={image} alt="This is a dish."/><br/>
+      <button onClick={() => {
+        let adjustedRecipes = [];
+        for (let i=0; i<recipes.length; i++) {
+          if (recipes[i].name != name) {
+            adjustedRecipes.push(recipes[i]);
+          }
+          setRecipes(adjustedRecipes);
+        }
+      }} >REMOVE</button>
     </div>
   );
 }
 
 
+{/* <div>
+<nav>
+  <Link to="/AddRecipe">Add Recipe</Link>
+</nav>
+<h1>Recipe List</h1>
+</div> */}
