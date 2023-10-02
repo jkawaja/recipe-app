36fee6c250d9e2aaa5019f0dd99d83f6a8a529ce
Justin Kawaja <jkawaja909@gmail.com>
2023-10-02 18:24:41 -0230

    adding functionality for API calls

--- recipe-backend/src/server.js
+++ recipe-backend/src/server.js
@@ -1,19 +1,45 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
+import bodyParser from 'body-parser';
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false}))
 
+// recipeData 
 
-// app.post('/hello', (req, res) =>{
+app.get('/api/recipes', async (req, res) => {
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+  const db = client.db('react-recipe-db');
 
-//   res.send(`Hello ${req.body.name}!`);
-// });
+  const data = await db.collection('recipes').find({}).toArray();
 
-// app.get('/hello/:name', (req, res) => {
-//   const { name } = req.params;
-//   res.send(`Hello ${name}!!`);
-// })
+  console.log(data);
+  res.json(data);
 
-app.listen(8000, () => {
-  console.log('Server is listening on port 8000');
+})
+
+app.post('/api/removeRecipe', async (req, res) => {
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+  const db = client.db('react-recipe-db');
+
+  const result = await db.collection('recipes').deleteOne({name:req.body.recipename});
+  console.log(result.deletedCount)
+
+  console.log(req.body.recipename);
+  let adjustedRecipes = [];
+  for (let i=0; i<recipeData.length; i++) {
+    if (recipeData[i].name != req.body.recipename) {
+      adjustedRecipes.push(recipeData[i]);
+    }
+  }
+  recipeData = adjustedRecipes;
+
+  res.sendStatus(200);
+})
+
+app.listen(4000, () => {
+  console.log('Server is listening on port 4000');
 })
