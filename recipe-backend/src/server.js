52590933915f06e88a04c4da16c650b97d35b5cb
Justin Kawaja <jkawaja909@gmail.com>
2023-10-03 20:39:16 -0230

    added api/recipeAdd coding, started multer

--- recipe-backend/src/server.js
+++ recipe-backend/src/server.js
@@ -2,7 +2,11 @@ import express from 'express';
 import { MongoClient } from 'mongodb';
 import bodyParser from 'body-parser';
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+
 const app = express();
+
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false}))
@@ -32,11 +36,25 @@ app.post('/api/addRecipe', async (req, res) => {
   const client = new MongoClient("mongodb://127.0.0.1:27017");
   await client.connect();
   const db = client.db('react-recipe-db');
-  const result = await db.collection('recipes').insertOne({name:req.body.recipename})
-  res.sendStatus(200);
+  const result = await db.collection('recipes').insertOne(
+    {name:req.body.name,
+    ingredients:req.body.ingredients,
+    directions:req.body.directions,
+    description:req.body.description,
+    image:req.body.image})
+  const data = await db.collection('recipes').find({}).toArray();
+  res.json(data);
 })
 
-app
+app.post('/api/addImage', upload.single('recipes'), async (req, res, next) => {
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+  const db = client.db('react-recipe-db');
+  const result = await.db.collection
+
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+})
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
